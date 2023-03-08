766ac543daa115cc4c2033b6b2c40c48e73b0066
Park Jisoo <coya319@naver.com>
2023-03-08 23:59:23 +0900

    feat(component scan-memberService): Use component scan

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     // 1. 인터페이스와 구현체를 MemberServiceImpl이 직접 주입하던 방식.
@@ -9,6 +13,7 @@ public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
 
     // 2-2. 생성자를 통해 memberRepository의 구현체로 무엇이 들어갈 지를 결정.
+    @Autowired // ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
