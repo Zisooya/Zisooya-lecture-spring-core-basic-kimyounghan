bb470313463171a4739e3a6a46410f69538b3fa3
Park Jisoo <coya319@naver.com>
2023-03-09 23:55:44 +0900

    feat(component scan): Specify the scope of the component scan

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,9 @@ import static org.springframework.context.annotation.ComponentScan.*;
 
 @Configuration
 @ComponentScan(
+
+//        basePackages = "hello.core",
+//        basePackageClasses = AutoAppConfig.class,
         // 예제 에러나지 않도록 애노테이션 빈 등록에서 뺄 타입을 정해주기.
         // (AppConfig는 @Configuration에 의해 빈을 수동으로 등록하는데, @Configuration에는 @Component가 포함되어 있기 때문. )
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
